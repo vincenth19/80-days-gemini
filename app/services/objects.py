397fdb097a890a3011e9a7b14fa1bb4cded6fd3c
Marcel Santoso <marcelsantosa98@gmail.com>
2024-07-17 21:53:56 +0000

    added objects to handle user and game saves

---
+++ app/services/objects.py
@@ -0,0 +1,49 @@
+class User():
+    def __init__(self,
+                 user_id,
+                 username,
+                 creation_time,
+                 last_updated,
+                 game_ids=[]):
+        self.user_id = user_id
+        self.username = username
+        self.creation_time = creation_time 
+        self.last_updated = last_updated
+        self.game_ids = game_ids
+
+    def to_dict(self):
+        return {"user_id": self.user_id,
+                "username": self.username,
+                "creation_time": self.creation_time,
+                "last_updated": self.last_updated,
+                "game_ids": self.game_ids}
+
+    @staticmethod
+    def from_dict(source):
+        return User(**source)
+
+
+class GameSaveData():
+    def __init__(self, id, user_id, title, 
+                 creation_time, last_updated, 
+                 days_left=80, journey_path=["London"]):
+        self.id = id
+        self.user_id = user_id
+        self.title = title
+        self.creation_time = creation_time 
+        self.last_updated = last_updated
+        self.days_left = days_left
+        self.journey_path = journey_path
+
+    def to_dict(self):
+        return {"game_id": self.id,
+                "user_id": self.user_id,
+                "title": self.title,
+                "creation_time": self.creation_time,
+                "last_updated": self.last_updated,
+                "days_left": self.days_left,
+                "journey_path": self.journey_path}
+
+    @staticmethod
+    def from_dict(source):
+        return GameSaveData(**source)
