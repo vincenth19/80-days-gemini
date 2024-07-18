7b0ea9ffb21718b5e5e2a1510104a10935e32930
Marcel Santoso <marcelsantosa98@gmail.com>
2024-07-18 22:59:17 +0000

    added basic game api endpoints

--- app/api/v1/routers/game.py
+++ app/api/v1/routers/game.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter, HTTPException
+import os
+from fastapi import APIRouter, HTTPException, Query
 from fastapi.responses import StreamingResponse
 from app.utils.file import read_yaml_file
 from app.services.gemini import GeminiClient
+from app.services.database import FirestoreClient
+from loguru import logger
+
+db_client = FirestoreClient(os.getenv('PROJECT_ID'), logger=logger)
 
 router = APIRouter(
     prefix="/game",
@@ -32,3 +37,19 @@ async def start_game():
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail=str(e))
+
+@router.post("/{user_id}")
+async def new_game(user_id: str, title: str = Query("Unnamed Game", min_length=1)):
+    return db_client.create_new_game(user_id=user_id, title=title)
+
+@router.get("/{user_id}")
+def list_saves(user_id):
+    return db_client.retrieve_user_data_ref(user_id).get().get('game_ids')
+
+@router.get("/{user_id}/{game_id}")
+def retrieve_save(user_id, game_id):
+    return db_client.retrieve_game_data_ref(game_id).get().to_dict()
+
+@router.delete("/{user_id}/{game_id}")
+def delete_save(user_id, game_id):
+    return db_client.delete_game_save(user_id, game_id)
--- app/services/database.py
+++ app/services/database.py
@@ -1,8 +1,10 @@
 import os
 import logging
 from uuid import uuid4
-from objects import User,GameSaveData
+from app.services.objects import User,GameSaveData
+# from objects import User, GameSaveData
 from google.cloud import firestore
+from loguru import logger
 
 FIREBASE_DB_NAME = os.getenv('FIREBASE_DB_NAME')
 USERS_COLLECTION = os.getenv('USERS_COLLECTION')
@@ -17,31 +19,56 @@ class FirestoreClient():
         # This assumes that we use firebase authentication that should give unique user_id
         return self.client.collection(USERS_COLLECTION).document(user_id)
     
-    def retrieve_game_data_ref(self, game_id):
-        return self.client.collection(GAME_SAVES_COLLECTION).document(game_id)
-    
     def add_new_user(self, user_id, username):
+        self.logger.info(f'adding new user')
         user = User(user_id, username, 
                     creation_time=firestore.SERVER_TIMESTAMP, 
                     last_updated=firestore.SERVER_TIMESTAMP, 
                     game_ids=[])
-        update_time, user_doc_ref = self.client.collection(USERS_COLLECTION).add(user.to_dict())
-        self.logger.info(f'added new user')
-        return user_doc_ref.get()
+        self.logger.info(f'adding new user to user collection')
+        user_doc_ref = self.client.collection(USERS_COLLECTION).document(user.id)
+        user_doc_ref.set(user.to_dict())
+        return user_doc_ref.get().to_dict()
+    
+    def retrieve_game_data_ref(self, game_id):
+        return self.client.collection(GAME_SAVES_COLLECTION).document(game_id)
 
     def create_new_game(self, user_id, title):
-        user_doc_ref = self.retrieve_user_data_ref(user_id)
-        user_doc = user_doc_ref.get()
         game = GameSaveData(id = str(uuid4()), 
                             user_id=user_id, title=title, 
                             creation_time=firestore.SERVER_TIMESTAMP, 
                             last_updated=firestore.SERVER_TIMESTAMP)
-        self.client.collection(GAME_SAVES_COLLECTION).document(game.id).set(game.to_dict())
+        user_doc_ref = self.retrieve_user_data_ref(user_id)
+        user_doc = user_doc_ref.get()
         updated_game_ids = user_doc.get('game_ids') + [game.id]
+
+        self.logger.info(f"adding new game id {game.id} to game save collection")
+        game_doc_ref = self.client.collection(GAME_SAVES_COLLECTION).document(game.id)
+        game_doc_ref.set(game.to_dict())
+        self.logger.info(f"adding new game id {game.id} to user data")
         user_doc_ref.update({'last_updated': firestore.SERVER_TIMESTAMP, 'game_ids': updated_game_ids})
+        return game_doc_ref.get().to_dict()
+    
+    def delete_game_save(self, user_id, game_id):
+        self.client.collection(GAME_SAVES_COLLECTION).document(game_id).delete()
+        user_doc_ref = self.retrieve_user_data_ref(user_id)
+        user_doc = user_doc_ref.get()
+        updated_game_ids = user_doc.get('game_ids')
+        self.logger.info(f'removing {game_id} from user data and game save collection')
+        updated_game_ids.remove(game_id)
+        user_doc_ref.update({'last_updated': firestore.SERVER_TIMESTAMP, 'game_ids': updated_game_ids})
+        return {"game_id": game_id}
 
     def update_path(self, game_id, latest_city):
         game_doc_ref = self.retrieve_game_data_ref(game_id=game_id)
         game_doc = game_doc_ref.get()
+        self.logger.info(f'updating path in game {game_id}')
         updated_journey_path = game_doc.get('journey_path') + [latest_city]
         game_doc_ref.update({'last_updated': firestore.SERVER_TIMESTAMP, 'journey_path': updated_journey_path})
+
+# if __name__=="__main__":
+#     db_client = FirestoreClient('days-gemini', logger=logger)
+#     # test = db_client.add_new_user('test1', 'test1')
+#     # test = db_client.create_new_game('test1', 'test1')
+#     # test = db_client.delete_game_save('test1', 'a1c22f3c-b651-439f-9427-244d044dc970')
+#     print(test)
